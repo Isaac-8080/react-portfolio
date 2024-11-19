613f09f6605efb3c48baea022a2af7546d0555c9
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-11-19 13:05:47 +0000

    Review updated

---
+++ src/components/Reviews/ReviewItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ReviewItem = () => {
+  return (
+    <>
+      
+      <div className="flex-none w-[400px] h-[220px] p-4 rounded-lg bg-secondaryColor text-[#FFFFFF] md:shadow-2xl italic">
+        "Isaac is not only a talented developer but also reliable and great at keeping us updated throughout the project. His skills in JavaScript, React, and database management were key to the success of our project. We’re looking forward to working with him again on future projects."
+      </div>
+        
+    </>
+  )
+}
+
+export default ReviewItem
