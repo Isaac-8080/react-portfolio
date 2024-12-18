23c5180098827d7567cd8cee5a6525edcad77cf1
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-12-18 12:39:31 +0000

    Reviews updated

--- src/components/Reviews/ReviewItem.jsx
+++ src/components/Reviews/ReviewItem.jsx
@@ -6,7 +6,7 @@ const ReviewItem = (props) => {
       <div className="flex-none w-[400px] h-[230px] md:h-[250px] p-4 rounded-lg bg-secondaryColor shadow text-[#FFFFFF] md:shadow-2xl">
         <div className='flex flex-col gap-2'>
           <div className='flex flex-row gap-3 items-center'>
-            <img src={props.image} className='w-16 h-fit bg-white rounded-full' />
+            <img src={props.image} className='w-16 h-16 bg-white rounded-full' />
             <div>
               <p className='ml-5 font-bold'>{props.name}</p>
               <p className=''>{'— ' + props.profession}</p>
