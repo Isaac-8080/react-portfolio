d981c68e81a79f4f36e74d86b52969327a8977c9
Isaac Opoku <isaacmanuopoku@gmail.com>
2025-01-15 03:16:28 +0000

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero';
-import Projects from './components/project/Projects';
 
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import 'aos/dist/aos.css' // You can also use <link> for styles;
 import AboutEducation from './components/AboutEducation/AboutEducation';
+import Projects from './components/project/Projects';
 import SkillsTechnologies from './components/SkillsTechnologies/SkillsTechnologies';
 import Review from './components/Reviews/Review';
 import Footer from './components/Footer';
-// ..
-AOS.init();
 
 const App = () => {
 
+  AOS.init();
+  
   return (
     <>
       <Navbar />
